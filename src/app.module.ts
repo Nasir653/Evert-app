b9a0a2c52651ad69662e168ab8cf1c55d524ed09
Nasir <malikaadi653@gmail.com>
2025-09-22 23:27:23 +0530

    integrated swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { UsersModule } from './modules/users/users.module';
-import { VendorsModule } from './vendors/vendors.module';
 import { AdminModule } from './admin/admin.module';
-import { AdminModule } from './src/admin/admin.module';
-import { UsersModule } from './users/users.module';
-import { UsersModule } from './modules/users/users.module';
+import { VendorsModule } from './vendors/vendors.module';
+
 
 @Module({
   imports: [UsersModule, AdminModule, VendorsModule],
